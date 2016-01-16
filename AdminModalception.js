798ef8f8ddeb99d52b594d80f7cf950b08205732
thetuningspoon <mike@thetuningspoon.com>
2016-01-16 11:22:31 -0500

    Initial commit

---
+++ AdminModalception.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    // Stack modal windows
+    $(document).on('pw-modal-opened', function() {
+        console.log('modal opened');
+        if(window !== top) { // The parent window is also a child window
+            console.log('Parent window is also a child window.');
+            var $iframe = $(this).find('iframe.pw-modal-window');
+            //var $dialogWrapper = $iframe.closest('.ui-dialog');
+            //var $contents = $iframe.contents();
+            var $parentDoc = $(parent.document);
+            var $parentIframe = $parentDoc.find('iframe.pw-modal-window');
+            var $parentDialogWrapper = $parentIframe.closest('.ui-dialog');
+
+            $parentDialogWrapper.addClass('modalception-full');
+
+
+        }
+    });
+    $(document).on('pw-modal-closed', function() {
+        if(window !== top) { // The parent window is also a child window
+            var $iframe = $(this).find('iframe.pw-modal-window');
+            //var $dialogWrapper = $iframe.closest('.ui-dialog');
+            //var $contents = $iframe.contents();
+            var $parentDoc = $(parent.document);
+            var $parentIframe = $parentDoc.find('iframe.pw-modal-window');
+            var $parentDialogWrapper = $parentIframe.closest('.ui-dialog');
+
+            $parentDialogWrapper.removeClass('modalception-full');
+        }
+    });
+
+});
